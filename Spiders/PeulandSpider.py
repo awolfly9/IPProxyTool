549f6e590cf158fe62158c5ce3cbdbe46e22c5e0
awolfly9 <awolfly9@gmail.com>
2016-12-31 17:25:21 +0800

    添加抓取 Peuland 的爬虫

---
+++ Spiders/PeulandSpider.py
@@ -0,0 +1,121 @@
+#-*- coding: utf-8 -*-
+
+import json
+import logging
+
+import requests
+import base64
+from Proxy import Proxy
+from utils import log
+from Spider import Spider
+
+
+class PeulandSpider(Spider):
+    def __init__(self, queue):
+        super(PeulandSpider, self).__init__(queue)
+        self.name = 'PeulandSpider'
+        self.urls = ['https://proxy.peuland.com/proxy_list_by_category.htm']
+        self.timeout = 20
+
+    def run(self):
+        for i, url in enumerate(self.urls):
+            try:
+                r = requests.get(url = url, timeout = self.timeout)
+                self.write(r.text.encode('utf-8'))
+                self.parse_page(r)
+            except Exception, e:
+                log('spider run %s Exception:%s' % (self.name, str(e)), logging.WARNING)
+
+    def parse_page(self, r):
+        headers = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Host': 'proxy.peuland.com',
+            'Referer': 'https://proxy.peuland.com/proxy_list_by_category.htm',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:50.0) Gecko/20100101 Firefox/50.0',
+            'X-Requested-With': 'XMLHttpRequest',
+        }
+
+        cookies = {
+            'peuland_id': '649e2152bad01e29298950671635e44a',
+            'CNZZDATA1253154494': '179614273-1483168549-%7C1483168549',
+            'peuland_md5': '9b941affd9b676f62ab93081f6cc9a1b',
+            'w_h': '800',
+            'w_w': '1280',
+            'w_cd': '24',
+            'w_a_h': '709',
+            'w_a_w': '1280',
+            'PHPSESSID': '5g8o2ubj6t4fenh2dklnrcr6n4',
+            'php_id': '1288641658',
+        }
+
+        for i in range(1, 9):
+            fromdata = {
+                'country_code': '',
+                'is_clusters': '',
+                'is_https': '',
+                'level_type': '',
+                'search_type': 'all',
+                'type': '',
+                'page': i,
+            }
+
+            url = 'https://proxy.peuland.com/proxy/search_proxy.php'
+
+            req = None
+            ret = False
+            for j in range(3):
+                try:
+                    req = requests.post(url = url, headers = headers, data = fromdata, cookies = cookies, timeout = 50)
+                    log('PeulandSpider parse_page req.text:%s' % req.url)
+                    ret = True
+                    break
+                except Exception, e:
+                    log('PeulandSpider parse_page exception:%s' % str(e), logging.WARNING)
+                    continue
+
+            if ret == False:
+                continue
+
+            result = json.loads(req.text)
+            items = result.get('data')
+            for item in items:
+                ip = base64.b64decode(item.get('ip'))
+                port = base64.b64decode(item.get('port'))
+                type = base64.b64decode(item.get('type'))
+                https = base64.b64decode(item.get('is_https'))
+                level_type = base64.b64decode(item.get('level_type'))
+                time_total = base64.b64decode(item.get('time_total'))
+                time_downloadspeed = base64.b64decode(item.get('time_downloadspeed'))
+                country = base64.b64decode(item.get('country'))
+                country_zw = base64.b64decode(item.get('country_zw'))
+                status_cnc = base64.b64decode(item.get('status_cnc'))
+                status_ctn = base64.b64decode(item.get('status_ctn'))
+
+                # log('type:%s, https:%s, level_type:%s, time_total:%s, time_downloadspeed:%s, country:%s,
+                # country_zw:%s'
+                #     'status_cnc:%s, status_ctn:%s' % (type, https, level_type, time_total, time_downloadspeed,
+                # country, country_zw, status_cnc, status_ctn))
+
+                proxy = Proxy()
+                proxy.set_value(
+                        ip = ip,
+                        port = port,
+                        country = country_zw,
+                        anonymity = level_type,
+                        https = https,
+                        speed = '-1',
+                )
+
+                log('PeulandSpider run proxy:%s' % str(proxy))
+
+                self.add_proxy(proxy)
+
+
+if __name__ == '__main__':
+    queue = []
+    spider = PeulandSpider(queue)
+    spider.run()
